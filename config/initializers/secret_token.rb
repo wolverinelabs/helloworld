e67b9243ae2b46dd4088e56857f078c306a28e0d
Junhao Jiang <junhao.john.jiang@gmail.com>
2013-03-22 00:53:21 -0700

    Initial commit of a basic Ruby on Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '8ace6656ec0f8de68d15926a531bfbd3addb04c7f5a1eb349a755e27e58458d96bf9f5e4975174be36ccae9ba2622384b3ebc337c3b68ddde650c8fb6b919029'
